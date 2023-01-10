56c1501cff123e7341a26478e38e9b0f69a22a81
Hugo Andrade <habp@ecomp.poli.br>
2023-01-10 03:27:52 -0300

    Minor updates on GUI.

--- src/main/java/com/example/battleship/MenuController.java
+++ src/main/java/com/example/battleship/MenuController.java
@@ -3,6 +3,7 @@ package com.example.battleship;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Node;
@@ -12,23 +13,37 @@ import java.io.IOException;
 
 public class MenuController extends BattleshipMain {
 
-    private Scene scene2;
+    private Scene scene;
     private FXMLLoader fxmlLoader;
     private Stage stage;
 
+    // Displays a new scene, which consists of the games' grid.
     public void changeScene(ActionEvent event) throws IOException {
         fxmlLoader = new FXMLLoader(BattleshipMain.class.getResource("grid-view.fxml"));
-        scene2 = new Scene(fxmlLoader.load(), 800, 500);
+        scene = new Scene(fxmlLoader.load(), 800, 500);
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene2);
+        stage.setScene(scene);
         stage.show();
     }
-    public void rulesButtonPressed(ActionEvent event) throws IOException{
+
+    // Adds rules text on the right side of the panel.
+    public void onMouseEnteredRules(MouseEvent event) throws IOException{
 
         fxmlLoader = new FXMLLoader(BattleshipMain.class.getResource("menuRules-view.fxml"));
-        scene2 = new Scene(fxmlLoader.load(), 800, 500);
+        scene = new Scene(fxmlLoader.load(), 800, 500);
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        stage.setScene(scene2);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+
+    // Returns to menu's standard state (without rule's text).
+    public void onMouseEnteredStartGame(MouseEvent event) throws IOException{
+
+        fxmlLoader = new FXMLLoader(BattleshipMain.class.getResource("menu-view.fxml"));
+        scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
         stage.show();
 
     }
